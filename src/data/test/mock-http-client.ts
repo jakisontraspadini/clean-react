b88f03d75b71a6f15087a04a55c8285bec2f4fc1
Jakison Traspadini <jakison.traspadini@gmail.com>
2022-08-08 10:12:38 -0300

    refactor: group imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,5 +1,4 @@
-import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
+import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
@@ -8,7 +7,7 @@ export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
     statusCode: HttpStatusCode.ok
   }
 
-  async post(params: HttpPostParams<T>): Promise<HttpResponse<R>> {
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,11 @@
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
-import { InvalidCredentialsError } from '@/domain/erros/invalid-credentials-error'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { UnexpectedError } from '@/domain/erros/invalid-unexpected-error'
+import { HttpPostClientSpy } from '@/data/test'
+import { mockAccountModel, mockAuthentication } from '@/domain/test'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/erros'
+import { AuthenticationParams } from '@/domain/usecases'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AccountModel } from '@/domain/models'
 import faker from 'faker'
-import { AccountModel } from '@/domain/models/account-model'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
