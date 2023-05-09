585516e1790152441a10a17dc0f322aa4a0df9ee
Jakison Traspadini <jakisontraspadini@MacBook-Pro-de-Jakison.local>
2023-05-09 20:27:18 -0300

    refactor: change imports to use module

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,6 @@
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { faker } from '@faker-js/faker'
-import { mockAuthentication } from '../../../domain/test/mock-authentication'
-import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 
 type SutTypes = {
@@ -25,7 +25,7 @@ describe('RemoteAuthentication', () => {
 
   test('Should call HttpClient with correct body', async () => {
     const { sut, httpPostClientSpy } = makeSut()
-    const authenticationParams = mockAuthentication();
+    const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
